e22fb54c96fb212a86f9c1e6dd01992955f0f7eb
Nawin P <nawinc.work@gmail.com>
2020-01-12 22:37:14 +0700

    [Web][Landing Page] Fixed brand icon

--- src/component/Brands.js
+++ src/component/Brands.js
@@ -15,6 +15,15 @@ export default Brand
 const BrandText = styled.h2`
   margin: auto 0;
   margin-right: 1em;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 22px
+  color: #43BEF2;
+  & span {
+    color: #000000;
+    font-weight: normal;
+  }
 `;
 
 const Image = styled.img`
